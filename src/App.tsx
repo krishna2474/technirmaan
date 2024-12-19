e113c633bec05f998bb626580f326ca18fac5264
Krishna <krishnakukreja85@gmail.com>
2024-12-19 20:46:06 +0530

    Added Scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { Landing } from "./pages/Landing";
 import { Registration } from "./pages/Registration";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/register/:eventId" element={<Registration />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/register/:eventId" element={<Registration />} />
+      </Routes>
+    </>
   );
 }
 
