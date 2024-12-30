32908504355cab1a17579db3fd68cc18c3449ce0
Krishna <krishnakukreja85@gmail.com>
2024-12-30 17:16:48 +0530

    Payment

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ContactUs = () => {
   return (
     <div className="max-w-3xl mx-auto p-4">
--- src/pages/TermsAndConditions.tsx
+++ src/pages/TermsAndConditions.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const TermsAndConditions = () => {
   return (
     <div className="max-w-3xl mx-auto p-4">
